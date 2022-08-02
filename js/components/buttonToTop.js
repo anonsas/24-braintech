47ac8836a5c4b7e67aff4cd3d2c1a7fa085a94e8
Igor Lukjanov <lukjanov.igr@gmail.com>
2022-08-02 09:32:56 +0300

    Add buttonToTop + css refactoring

---
+++ js/components/buttonToTop.js
@@ -0,0 +1,15 @@
+const moveToTopBtn = document.querySelector('.btn-to-top');
+
+window.addEventListener('scroll', (e) => {
+  console.log(window.scrollY);
+  if (window.scrollY > 300) {
+    moveToTopBtn.classList.add('btn-to-top-show');
+  } else {
+    moveToTopBtn.classList.remove('btn-to-top-show');
+  }
+});
+
+moveToTopBtn.addEventListener('click', () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0;
+});
